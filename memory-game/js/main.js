6bf60432fcaa191e731fc7699198d9431d808a93
keerthi <velaga88@gmail.com>
2019-09-24 18:08:51 -0400

    commit game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -14,28 +14,37 @@ const cards = [
 const cardsInPlay = [];
 function checkForMatch()
 {
-if (cardsInPlay[0] === cardsInPlay[1])
- {
-  console.log("You found a match!");
-} 
-else {
-  console.log("Sorry, try again.");
+     if (cardsInPlay[0] === cardsInPlay[1])
+      {
+        alert("You found a match!");
+       } 
+      else 
+      {
+        alert("Sorry, try again.");
+       }  
 }
-}
-function flipCard(cardId)
+ function flipCard()
 { 
-	checkForMatch();
-	if (cardsInPlay === 2)
-		{ alert("you found a match");
-        }
-        else{
-        	alert("sorry try again");
-        }
+	const cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if(cardsInPlay.length === 2){
+	checkForMatch();}
+	if(cardsInPlay.length === 4){
+		checkForMatch();}
 	console.log(" User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+	console.log(cards[cardId].suit);	
+}        	
 	
+function createBoard()
+{
+	for(var i = 0; i < cards.length; i ++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
 }
-flipCard(0);
-flipCard(2);
+}
+createBoard();
